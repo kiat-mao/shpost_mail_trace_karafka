01284d78cd4d08aaeb16356c03aa7f202f2d6ec2
kiat <kiat.mao@gmail.com>
2020-12-28 09:17:17 +0800

    create project

---
+++ karafka_server_start.sh
@@ -0,0 +1,37 @@
+count=$1
+
+int=1
+
+if [ -z $count ];then
+   count=4
+fi
+
+
+if [ -e "./tmp/pids/karafka_server" ];then
+   echo "Karafka server already started. Please shutdown first."
+   exit
+fi 
+
+while(( $int<= count ))
+do
+   echo $int
+   let "int++"
+
+   if [ -e "./tmp/pids/karafka" ];then 
+      rm ./tmp/pids/karafka
+   fi 
+   
+   bundle exec karafka s -d
+   # sleep(5)
+   while ( [ ! -e "./tmp/pids/karafka" ] )
+   do
+      sleep 1
+      # echo "sleep 1"
+   done
+   cat ./tmp/pids/karafka >> ./tmp/pids/karafka_server
+   echo '' >> ./tmp/pids/karafka_server
+
+   # rm ./tmp/pids/karafka
+
+   sleep 1
+done
---
+++ karafka_server_stop.sh
@@ -0,0 +1,19 @@
+
+if [ -e "./tmp/pids/karafka_server" ];then 
+   cat ./tmp/pids/karafka_server|while read LINE 
+   do
+      if [ -n $LINE ];then
+         echo $LINE
+         kill -2 $LINE
+         sleep 1
+      fi
+   done
+fi 
+
+if [ -e "./tmp/pids/karafka_server" ];then 
+   rm ./tmp/pids/karafka_server
+fi 
+
+if [ -e "./tmp/pids/karafka" ];then 
+   rm ./tmp/pids/karafka
+fi
